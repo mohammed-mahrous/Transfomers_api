7f81431f5de1f36393e7ff77fd08046bba53a307
mohamed-mahrous <xxspider6@gmail.com>
2024-06-11 11:22:16 +0300

    float-16

--- TransformersProcessor.py
+++ TransformersProcessor.py
@@ -1,28 +1,24 @@
 from transformers import SeamlessM4Tv2ForTextToSpeech , AutoProcessor 
-import torch , numpy as np
+import torch , scipy, numpy as np
 
 
 class TransformersProcessor:
     def __init__(self) -> None:
         self.model = SeamlessM4Tv2ForTextToSpeech.from_pretrained("facebook/seamless-m4t-v2-large",torch_dtype=torch.float16)
         self.processor = AutoProcessor.from_pretrained("facebook/seamless-m4t-v2-large")
-        
+        self.device = torch.device('cuda')
+        self.model.to(self.device)
 
     def process(self,message:str) -> bytes:
-        device = torch.device('cuda')
-        self.model.to(device)
-        text_inputs = self.processor(text = message, src_lang="arb", return_tensors="pt").to(device)
+        text_inputs = self.processor(text = message, src_lang="arb", return_tensors="pt").to(self.device)
         audio_array_from_text = self.model.generate(**text_inputs, tgt_lang="arb",speaker_id=10)[0].cpu().numpy().squeeze()
-        return audio_array_from_text
+        return audio_array_from_text.astype(np.float32)
 
     def ProcessAndWriteFile(self,message:str, file_path:str="out_from_text.wav") -> None:
-        import scipy
-        device = torch.device('cuda')
-        self.model.to(device)
-        text_inputs = self.processor(text = message, src_lang="arb", return_tensors="pt").to(device)
+        text_inputs = self.processor(text = message, src_lang="arb", return_tensors="pt").to(self.device)
         audio_array_from_text = self.model.generate(**text_inputs, tgt_lang="arb",speaker_id=10)[0].cpu().numpy().squeeze()
         sample_rate = self.model.config.sampling_rate
-        scipy.io.wavfile.write(file_path, rate=16000, data=audio_array_from_text)
+        scipy.io.wavfile.write(file_path, rate=16000, data=audio_array_from_text.astype(np.float32))
 
 
 
